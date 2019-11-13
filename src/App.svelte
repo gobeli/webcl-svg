aed9e97182e6bc2d55be9554174edac990f8b68f
gobeli <etienne.gobeli@students.fhnw.ch>
2019-11-13 17:39:00 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Eye from './Eye.svelte';
+</script>
+
+<div>
+	<h1 style="margin-left: 3em;">I see where you go...</h1>
+	<Eye left="100px" />
+	<Eye left="250px" />
+</div>
---
+++ src/Eye.svelte
@@ -0,0 +1,75 @@
+<style>
+	svg {
+		width:  100px;
+		height: 80px;
+		top:  100px;
+		position: absolute;
+	}
+</style>
+
+<script>
+  let closed = false, rectEl;
+  let eyeOffset = {
+    x: 0,
+    y: 0
+  }
+
+  // setInterval(() => {
+  //   closed = true;
+  //   setTimeout(() => closed = false, 400);
+  // }, 2400)
+
+  const onMouseMove = evt => {
+    const rect = rectEl.getBoundingClientRect();
+    const xo = rect.x + rect.width/2;  // x-origin
+    const yo = rect.y + rect.height/2; // y-origin
+    const xm = evt.clientX - xo; // the normalized x/y coords to work with
+    const ym = evt.clientY - yo;
+    const xmax = rect.width/1.5;
+    const ymax = rect.height/2;
+    const widestFocus = 400; // when x is so far away, the eye is maximal extended
+    const scaledX = xm * (xmax / widestFocus );
+    let xe = xm > 0
+                ? Math.min( xmax, scaledX)
+                : Math.max(-xmax, scaledX);
+    const scaledY = ym * (ymax / widestFocus );
+    let ye = ym > 0
+                ? Math.min( ymax, scaledY)
+                : Math.max(-ymax, scaledY);
+    if (xe*xe + ye*ye > xmax * ymax) {
+        xe *= 0.9;
+        ye *= 0.9;
+    }
+    eyeOffset = { x: xe, y: ye };
+  }
+
+  export let left;
+</script>
+<svelte:window on:mousemove={onMouseMove}/>
+
+<svg viewBox="0 0 120 120" style="left: {left}" on:click={() => closed = !closed}>
+  <filter id="shadow">
+    <feGaussianBlur in="SourceAlpha" stdDeviation="3" />
+    <feOffset       dx="0" dy="8" />
+    <feColorMatrix  type="matrix"
+                    values="0 0 0 0  0
+                            0 0 0 0  0
+                            0 0 0 0  0
+                            0 0 0 .5 0"/>
+    <feBlend        in="SourceGraphic" mode="normal"/>
+  </filter>
+  <radialGradient id="gradient1" gradientUnits="objectBoundingBox" cx="50%" cy="50%" r="50%">
+    <stop offset= "38%" stop-color="#000000" stop-opacity="1" />
+    <stop offset= "46%" stop-color="#073F80" stop-opacity="1" />
+    <stop offset= "90%" stop-color="#8EC0DD" stop-opacity="1" />
+    <stop offset="100%" stop-color="#2F3A46" stop-opacity="1" />
+  </radialGradient>
+  <g class="iris" style="transform: translateX({eyeOffset.x}px) translateY({eyeOffset.y}px)">
+    <ellipse bind:this={rectEl} cx="60" cy="60" rx="30" ry="30" opacity="1" fill="url(#gradient1)" />
+    <ellipse cx="50" cy="50" rx="7" ry="7" opacity="1" fill="#FFFFFF" fill-opacity="0.8"/>
+  </g>
+  <g class="lids">
+    <path d="M0 60 A60,60 0 0,1 120,60 A60,{closed ? 40 : 30} 0 0,{closed ? 1 : 0} 0,60 Z" fill="#FDDC99" fill-opacity="1" filter="url(#shadow)" />
+    <path d="M0 60 A60,60 0 0,0 120,60 A60,40 0 0,1 0,60 Z" fill="#F4CB76" fill-opacity="1" />
+  </g>
+</svg>
